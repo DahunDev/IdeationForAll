97e617a8c3eb7545f91a710bb2bd23335d57c1f6
Antonio Proa <antoniorproa@gmail.com>
2025-02-03 17:41:32 -0600

    Post-it content loading and drag bug fix

--- frontend/src/components/functions/PostIts.jsx
+++ frontend/src/components/functions/PostIts.jsx
@@ -1,36 +1,47 @@
 import { useState, useRef } from "react";
 
-export default function PostIt({ id, name, onClose }) {
+export default function PostIt({id, boardID, name, userID, content, groupID, imageLink, font, position, size, onClose }) {
   const [move, setMove] = useState(false);
   const postitRef = useRef();
+
 
   const [dx, setDx] = useState(0);
   const [dy, setDy] = useState(0);
 
+    function setPosition() {
+        postitRef.current.style.left = position.x + "px";
+        postitRef.current.style.right = position.x + "px";
+    }
   function mouseUp() {
-    setMove(false);
+      setMove(false);
+      console.log("stop moving");
   }
   function mouseDown(e) {
     setMove(true);
     const coordinates = postitRef.current.getBoundingClientRect();
     setDx(e.clientX - coordinates.x);
-    setDy(e.clientY - coordinates.y);
+      setDy(e.clientY - coordinates.y);
+      console.log("start moving");
   }
 
   function mouseMove(e) {
     if (move) {
-      const x = e.clientX - dx;
-      const y = e.clientY - dy;
-      postitRef.current.style.left = x + "px";
-      postitRef.current.style.top = y + "px";
+      const fx = e.clientX - dx;
+      const fy = e.clientY - dy;
+      postitRef.current.style.left = fx + "px";
+        postitRef.current.style.top = fy + "px";
+        position.x = fx;
+        position.y = fy;
+        console.log("post-it moved");
+      }
     }
-  }
 
   return (
-    <div className="post-it" ref={postitRef}>
+      <div className="post-it" ref={postitRef} onLoad={setPosition}>
       <div
         className="post-it-header"
         onMouseDown={mouseDown}
+        onMouseLeave={mouseUp}
         onMouseUp={mouseUp}
         onMouseMove={mouseMove}
       >
@@ -40,7 +51,7 @@ export default function PostIt({ id, name, onClose }) {
           &times;
         </div>
       </div>
-      <textarea cols="30" rows="10"></textarea>
+          <textarea cols="30" rows="10">{content}</textarea>
     </div>
   );
 }
