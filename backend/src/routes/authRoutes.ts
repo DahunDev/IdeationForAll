4623fe36df73414e2bdaa38bd5bf7d8696187b66
Jafar Albdeiri <jafaralbdeiri@my.unt.edu>
2025-02-15 14:00:33 -0600

    empty my branch

--- backend/src/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-// src/routes/authRoutes.ts
-import { Router } from "express";
-import { registerUserController } from "../controllers/authController";
-
-const router = Router();
-
-router.post("/register", registerUserController);
-
-export default router;
